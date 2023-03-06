ff8fe9df813fb5d9d25682309a768abc579dddec
K3NW48 <noThanks>
2023-03-06 05:40:55 -0500

    Im so happy!

--- boozhoo_bot.py
+++ boozhoo_bot.py
@@ -1,52 +1,89 @@
-import speech_recognition as sr
+# Import the required libraries
+import gradio as gr
+import openai, config, subprocess
 import pyttsx3
-import openai
-import config
 
+# Set up OpenAI API key
 openai.api_key = config.OPENAI_API_KEY
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voices', voices[1].id)
+engine.setProperty('voices', voices[2].id)
 
-r = sr.Recognizer()
-mic = sr.Microphone(device_index=1)
+# Create a list of dictionary messages which will store the bot responses
+messages = [{"role": "system", "content": 'You are the Anishinaabe hero Nanaboozhoo. Not only do you answer with profound wisdom but you will continue the conversation by answering like this, Boozhoo: (your answer)'}]
 
-conversation = ""
-user_name = "Dan"
-bot_name = "John"
+full_transcript = []
+# Define a function that transcribes audio from the microphone, sends it to OpenAI's GPT-3 model 
+# and returns the chat transcript
+def transcribe(audio):
+    global messages
+    global full_transcript
+    
+    # Print the audio file for debugging purposes
+    print(audio)
 
-while True:
-    with mic as source:
-        print("\n Listening...")
-        r.adjust_for_ambient_noise(source, duration=0.2)
-        audio = r.listen(source)
-    print("no longer listening")
+    # Open the audio file and send it to OpenAI's transcription API
+    audio_file = open(audio, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    
+    full_transcript.append(transcript["text"])
+    # Print the transcription for debugging purposes
+    print(transcript)
 
-    try:
-        user_input = r.recognize_google(audio)
-    except:
-        continue
+    # Add the user's transcription to the messages list as a new dictionary
+    messages.append({"role": "user", "content": transcript["text"]})
 
-    prompt = user_name+":"+user_input + "\n"+bot_name+":"
-    conversation += prompt
-
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=conversation,
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
+    # Send the latest set of messages to OpenAI's GPT-3 model to get a response message
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
     )
+    
+    # For debugging purposes, print the response message
+    print("HERE IS THE RESPONSE" + "\n")
+    print(response)
 
-    response_str = response["choices"][0]["text"].replace("\n", "")
-    response_str =response_str.split(
-        user_name + ":" ,1)[0].split(bot_name+ ":",1)[0]
+    # Extract the latest system message from the response and add it as a new message to the messages list
+    system_message = response["choices"][0]["message"]
+    messages.append(system_message)
 
-    conversation+= response_str +"\n"
-    print(response_str)
+    # Create Dall-e specific prompt from response
+    dalle_prompt = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+        {"role": "user", "content": f'Summarize this text "{response["choices"][0]["message"]["content"]}" into a short and concise Dall-e2 prompt.'}
+        ]
+    )
+    # Prompt Dall-e and give URL to image
+    dalle_response = openai.Image.create(
+            prompt = dalle_prompt["choices"][0]["message"]["content"],
+            size="512x512"
+        )
+    image_url = dalle_response['data'][0]['url']
 
-    engine.say(response_str)
+
+
+    # Speak most recent response
+    engine.say(system_message['content'])
     engine.runAndWait()
+
+    # Combine all messages in the messages list to create a chat transcript
+    chat_transcript = ""
+    for message in messages:
+        if message['role'] != 'system':
+            chat_transcript += message['role'] + ": " + message['content'] + "\n\n"
+
+    print(chat_transcript)
+
+    # Return the chat transcript
+    return image_url, chat_transcript
+
+
+
+# Create a Gradio interface to capture audio input from the user, pass it to the transcribe function
+# and display the resulting chat transcript
+speech_interface = gr.Interface(fn=transcribe, inputs=gr.Audio(source="microphone", type="filepath"), outputs=["image", "text"], title="Just WOW")
+
+
+speech_interface.launch()
